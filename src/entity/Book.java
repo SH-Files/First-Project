5ca76204547f372e375433175678c6e319f264ba
SH-Files <72735178+SH-Files@users.noreply.github.com>
2021-02-10 20:07:38 +0100

    Add files via upload

---
+++ src/entity/Book.java
@@ -0,0 +1,43 @@
+package entity;
+
+public class Book
+{
+    private final int id;
+    private String title;
+
+    public Book(int id, String title)
+    {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId()
+    {
+        return this.id;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getTitle()
+    {
+        return this.title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
