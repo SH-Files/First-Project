c3f8ad2bc87758a4a4d89fd2edef2859c7c25bac
SH-Files <72735178+SH-Files@users.noreply.github.com>
2021-04-15 13:49:53 +0200

    Add files via upload

---
+++ src/test/java/com/example/first/FirstProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FirstProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+
+
+    }
+
+}
