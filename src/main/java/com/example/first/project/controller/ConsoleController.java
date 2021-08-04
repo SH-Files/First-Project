f7167e98e5230f3be0447ffd27a35705ea66783c
SH-Files <72735178+SH-Files@users.noreply.github.com>
2021-08-04 07:52:39 +0200

    Update ConsoleController.java

--- src/main/java/com/example/first/project/controller/ConsoleController.java
+++ src/main/java/com/example/first/project/controller/ConsoleController.java
@@ -14,15 +14,6 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 public class ConsoleController {
 
-    /*
-
-        200: HTTP OK
-        400: BAD REQUEST
-        404: NOT FOUND
-        500: INTERNAL SERVER ERROR
-
-     */
-
     private final StudentService studentService;
 
     @Autowired
