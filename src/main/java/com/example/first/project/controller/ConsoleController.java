f5e0ff3da2e081ce4e8ec46b9bdec52b2d794703
SH-Files <72735178+SH-Files@users.noreply.github.com>
2021-08-04 07:52:52 +0200

    Update ConsoleController.java

--- src/main/java/com/example/first/project/controller/ConsoleController.java
+++ src/main/java/com/example/first/project/controller/ConsoleController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-@Controller
 @RestController
 public class ConsoleController {
 
