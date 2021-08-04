a3e2147d234e25779871e8ca48fca4331e5dc1dd
SH-Files <72735178+SH-Files@users.noreply.github.com>
2021-08-04 08:02:31 +0200

    Update ConsoleController.java

--- src/main/java/com/example/first/project/controller/ConsoleController.java
+++ src/main/java/com/example/first/project/controller/ConsoleController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-
 public class ConsoleController {
 
     private final StudentService studentService;
